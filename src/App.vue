8eea92dfca7271fee3d470a6ec62fe99f12ef2eb
markSong <mark.song@kakaocorp.com>
2019-04-29 17:43:13 +0900

    아이프레임 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <iframe-test/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import IframeTest from "./components/iframeTest";
 
 export default {
   name: 'app',
   components: {
+    IframeTest,
     HelloWorld
   }
 }
@@ -18,11 +21,11 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
 }
 </style>
---
+++ src/components/iframeTest.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="header">
+            헤더영역
+        </div>
+        <div class="iframe-wrapper">
+            <iframe src="https://www.daum.net/" class="iframe"></iframe>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "iframeTest"
+    }
+</script>
+
+<style scoped>
+    .header{
+        height: 80px;
+        border-bottom: solid 2px;
+        background-color: white;
+    }
+    .iframe-wrapper{
+
+    }
+    .iframe{
+        width: 100%;
+        height: 100%;
+        top:0;
+        left:0;
+        position: absolute;
+        z-index: -1;
+    }
+</style>
